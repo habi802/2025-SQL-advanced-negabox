34958e81cf8890530adee39cab2b6568c46f5866
habi802 <shb8002@gmail.com>
2025-11-21 09:58:39 +0900

    [init] first

---
+++ src/test/java/kr/co/negaboxdummy/NegaboxDummyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.negaboxdummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegaboxDummyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
