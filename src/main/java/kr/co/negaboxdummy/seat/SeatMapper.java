cd23a8c000ad94e2f6d666cfbe1f8a37ccb31cb3
habi802 <shb8002@gmail.com>
2025-11-24 12:52:33 +0900

    [feat] 좌석 더미 데이터 생성하는 코드 추가

--- src/main/java/kr/co/negaboxdummy/seat/SeatMapper.java
+++ src/main/java/kr/co/negaboxdummy/seat/SeatMapper.java
@@ -1,9 +1,13 @@
 package kr.co.negaboxdummy.seat;
 
+import kr.co.negaboxdummy.seat.model.ScreenGetRes;
 import kr.co.negaboxdummy.seat.model.SeatPostReq;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface SeatMapper {
-    int save(SeatPostReq req);
+    List<ScreenGetRes> findScreenIdAndScreenType(); // 상영관 전체 조회
+    int save(SeatPostReq req); // 좌석 데이터 추가
 }
