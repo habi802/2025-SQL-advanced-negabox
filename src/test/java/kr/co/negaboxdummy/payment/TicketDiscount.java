40432a2d12ffab2ad9d4dfba13ac10426f5681d8
d-kjh <dkjh9942@gmail.com>
2025-11-27 12:41:39 +0900

    11/27 프로젝트 더미데이터 insert 및 update 중

---
+++ src/test/java/kr/co/negaboxdummy/payment/TicketDiscount.java
@@ -0,0 +1,14 @@
+package kr.co.negaboxdummy.payment;
+
+import kr.co.negaboxdummy.config.FakerConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.transaction.annotation.Transactional;
+
+public class TicketDiscount extends FakerConfig {
+
+    @Test
+    @Transactional
+    void insertTicketDiscount() {
+        
+    }
+}
